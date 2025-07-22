e8ae70b2c2d52c16d70a3bbe3181e342a801f7dc
Variedades Laga Llantas Y Más. <123280285+VariedadesLagaLlantasYMasEC@users.noreply.github.com>
2025-07-22 16:27:35 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -24,24 +24,3 @@ links.forEach(link => {
     setTimeout(() => logo.classList.remove('girando'), 1000);
   });
 });
-
-document.querySelectorAll('.menu a').forEach(link => {
-  link.addEventListener('click', e => {
-    e.preventDefault();
-
-    const targetId = link.getAttribute('href').substring(1);
-
-    document.querySelectorAll('.section').forEach(section => {
-      section.classList.remove('active');
-    });
-
-    const btnMenu = document.getElementById('btnMenu');
-const menu = document.getElementById('menu');
-
-btnMenu.addEventListener('click', () => {
-  menu.classList.toggle('open');
-});
-    const targetSection = document.getElementById(targetId);
-    targetSection.classList.add('active');
-  });
-});
